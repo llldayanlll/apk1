824af4883645e78ccd90d722b20829b4de8d0c06
llldayanlll <142857893+llldayanlll@users.noreply.github.com>
2025-12-21 02:03:22 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    kotlin("android") apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.detekt)
-    alias(libs.plugins.nexus.publish)
-    cleanup
-    base
+    `kotlin-dsl`
 }
 
-allprojects {
-    val GROUP: String by project
-    val VERSION: String by project
-    val USE_SNAPSHOT: String? by project
-    group = GROUP
-    version = if (USE_SNAPSHOT.toBoolean()) "$VERSION-SNAPSHOT" else VERSION
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
 }
 
-val detektFormatting = libs.detekt.formatting
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-subprojects {
-    apply {
-        plugin("io.gitlab.arturbosch.detekt")
-    }
-
-    detekt {
-        config.from(rootProject.files("config/detekt/detekt.yml"))
-    }
-
-    dependencies {
-        detektPlugins(detektFormatting)
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
-val NEXUS_USERNAME: String? by project
-val NEXUS_PASSWORD: String? by project
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-            username.set(NEXUS_USERNAME)
-            password.set(NEXUS_PASSWORD)
-        }
-    }
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+    implementation(libs.spotless.gradlePlugin)
+    implementation(libs.detekt.gradlePlugin)
+    implementation(libs.dokka.gradlePlugin)
+    implementation(libs.versions.gradlePlugin)
 }
