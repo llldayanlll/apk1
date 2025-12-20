664163bb386208f3e447dcaf36c9e76f9ae65e72
dayan sterk <dayan.sterk@gmail.com>
2025-12-21 03:38:59 +0600

    Fix Gradle repository configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
@@ -10,13 +10,6 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(layout.buildDirectory)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,13 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
         mavenCentral()
     }
 }
