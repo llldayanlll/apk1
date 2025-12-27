b200443c42e45a2bf68cdda397aff74f3ac4fb2b
dayan sterk <dayan.sterk@gmail.com>
2025-12-28 02:42:02 +0600

    FIX: Android 6 compatible multi-file uploader with OkHttp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
             isMinifyEnabled = false
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,4 +36,5 @@ kotlin {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0") // ✅ Added OkHttp
 }
